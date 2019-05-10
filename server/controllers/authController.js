727e5860fa9b6f4e979c0d888240b3d75d9544f8
Hans Thummel <enikath@gmail.com>
2019-05-10 03:59:15 -0500

    adding session cookie

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -91,12 +91,14 @@ const login = async (req, res) => {
       if (!isAuthenticated) {
         res.status(403).json("Incorrect username or password");
       } else {
-        console.log('add username and pass to session user obj next...')
+        console.log("add username and pass to session user obj next...");
         // req.session.user = {
         // isAdmin: user.is_admin,
         // id: user.id,
-        let userobj={username = existinguser.username,
-        password = req.body.password}
+        let userobj = {
+          username: existinguser.username,
+          password: req.body.password
+        };
         req.session.user = userobj;
         // picture: user.picture,
         // name: user.name,
